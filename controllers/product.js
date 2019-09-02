6c96513309e601f4a523ef531c20a89b6a7674d2
davelorino <davelorino@gmail.com>
2019-09-02 16:45:41 +1000

    update product/job post

--- controllers/product.js
+++ controllers/product.js
@@ -17,6 +17,19 @@ exports.productById = (req, res, next, id) => {
   });
 };
 
+exports.remove = (req, res) => {
+  let product = req.product; 
+  product.remove((err, deletedProduct) => {
+    if(err) {
+      res.status(400).json({
+        error: console.log(err)
+      });
+    }
+    res.json({"message": "Product deleted successfully."
+    });
+  });
+};
+
 exports.read = (req, res) => {
   req.product.photo = undefined;
   return res.json(req.product);
@@ -33,7 +46,7 @@ exports.create = (req, res) => {
     }
     // check that we have all necessary fields
     const {name, description, price, category, on_premises, skills_required} = fields;
-    if(!name || !description || !price || !category || !one_premises || !skills_required) {
+    if(!name || !description || !price || !category || !on_premises || !skills_required) {
       return res.status(400).json({
         error: "Some fields are missing! Please make sure you have entered all required fields."
       });    
@@ -58,6 +71,52 @@ exports.create = (req, res) => {
     });
   });
 };
+
+
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if(err) {
+      return res.status(400).json({
+        error: "Image could not be uploaded"
+      });
+    }
+    // check that we have all necessary fields
+    const {name, description, price, category, on_premises, skills_required} = fields;
+    if(!name || !description || !price || !category || !on_premises || !skills_required) {
+      return res.status(400).json({
+        error: "Some fields are missing! Please make sure you have entered all required fields."
+      });    
+    }
+    let product = req.product;
+    product = _.extend(product, fields);
+    if(files.photo) {
+      if(files.photo.size > 2000000){
+        return res.status(400).json({
+          error: "Image must be less than 2mb in size"
+        });
+      }
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+    product.save((err, result) => {
+      if(err){
+        return res.status(400).json({
+          error: console.log(err)
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
+
+
+
+
+
 
 
 
--- routes/product.js
+++ routes/product.js
@@ -2,12 +2,14 @@
 
 const express = require('express');
 const router = express.Router();
-const { create, productById, read } = require('../controllers/product');
+const { create, productById, read, remove, update } = require('../controllers/product');
 const { userById } = require('../controllers/user');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
 router.get('/product/:productId', read);
 router.post("/product/create/:userId", requireSignin, isAuth, isAdmin, create);
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update);
 
 router.param("userId", userById);
 router.param("productId", productById);
